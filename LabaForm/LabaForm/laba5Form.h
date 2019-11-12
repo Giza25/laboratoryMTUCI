33ac730e146efd56ce4fdaad510eddb07734607a
Giza25 <47832574+Giza25@users.noreply.github.com>
2019-11-12 19:53:57 +0300

    Add files via upload

--- LabaForm/LabaForm/laba5Form.h
+++ LabaForm/LabaForm/laba5Form.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Func.h"
+#include "cmath"
+
 namespace LabaForm {
 
 	using namespace System;
@@ -34,21 +36,34 @@ namespace LabaForm {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ backBtn;
+	protected:
+
 	private: System::Windows::Forms::Button^ exitBtn;
 	private: System::Windows::Forms::Label^ labaTitle;
 
 	private: System::Windows::Forms::Label^ taskLabel;
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::TextBox^ textBox3;
-	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::Label^ aLabel;
+	private: System::Windows::Forms::Label^ bLabel;
+	private: System::Windows::Forms::Label^ xLabel;
+	private: System::Windows::Forms::Label^ yLabel;
+
+
+
+
+	private: System::Windows::Forms::TextBox^ aTextBox;
+	private: System::Windows::Forms::TextBox^ bTextBox;
+	private: System::Windows::Forms::TextBox^ xTextBox;
+	private: System::Windows::Forms::TextBox^ yTextBox;
+	private: System::Windows::Forms::TextBox^ resultTextBox;
+
+
+
+
+
+	private: System::Windows::Forms::Button^ solveBtn;
+
 
 
 
@@ -68,34 +83,35 @@ namespace LabaForm {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(laba5Form::typeid));
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->backBtn = (gcnew System::Windows::Forms::Button());
 			this->exitBtn = (gcnew System::Windows::Forms::Button());
 			this->labaTitle = (gcnew System::Windows::Forms::Label());
 			this->taskLabel = (gcnew System::Windows::Forms::Label());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->aLabel = (gcnew System::Windows::Forms::Label());
+			this->bLabel = (gcnew System::Windows::Forms::Label());
+			this->xLabel = (gcnew System::Windows::Forms::Label());
+			this->yLabel = (gcnew System::Windows::Forms::Label());
+			this->aTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->bTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->xTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->yTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->resultTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->solveBtn = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// button1
+			// backBtn
 			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->backBtn->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(44, 709);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(414, 105);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"Назад";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &laba5Form::button1_Click);
+			this->backBtn->Location = System::Drawing::Point(44, 709);
+			this->backBtn->Name = L"backBtn";
+			this->backBtn->Size = System::Drawing::Size(414, 105);
+			this->backBtn->TabIndex = 0;
+			this->backBtn->Text = L"Назад";
+			this->backBtn->UseVisualStyleBackColor = true;
+			this->backBtn->Click += gcnew System::EventHandler(this, &laba5Form::backBtn_Click);
 			// 
 			// exitBtn
 			// 
@@ -143,76 +159,106 @@ namespace LabaForm {
 			this->pictureBox1->TabIndex = 4;
 			this->pictureBox1->TabStop = false;
 			// 
-			// label1
+			// aLabel
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(625, 429);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
-			this->label1->TabIndex = 5;
-			this->label1->Text = L"label1";
+			this->aLabel->AutoSize = true;
+			this->aLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->aLabel->Location = System::Drawing::Point(233, 433);
+			this->aLabel->Name = L"aLabel";
+			this->aLabel->Size = System::Drawing::Size(65, 39);
+			this->aLabel->TabIndex = 5;
+			this->aLabel->Text = L"a =";
 			// 
-			// label2
+			// bLabel
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(621, 500);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
-			this->label2->TabIndex = 6;
-			this->label2->Text = L"label2";
+			this->bLabel->AutoSize = true;
+			this->bLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->bLabel->Location = System::Drawing::Point(233, 530);
+			this->bLabel->Name = L"bLabel";
+			this->bLabel->Size = System::Drawing::Size(65, 39);
+			this->bLabel->TabIndex = 6;
+			this->bLabel->Text = L"b =";
 			// 
-			// label3
+			// xLabel
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(824, 416);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(35, 13);
-			this->label3->TabIndex = 7;
-			this->label3->Text = L"label3";
+			this->xLabel->AutoSize = true;
+			this->xLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->xLabel->Location = System::Drawing::Point(677, 433);
+			this->xLabel->Name = L"xLabel";
+			this->xLabel->Size = System::Drawing::Size(63, 39);
+			this->xLabel->TabIndex = 7;
+			this->xLabel->Text = L"x =";
 			// 
-			// label4
+			// yLabel
 			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(821, 495);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(35, 13);
-			this->label4->TabIndex = 8;
-			this->label4->Text = L"label4";
+			this->yLabel->AutoSize = true;
+			this->yLabel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->yLabel->Location = System::Drawing::Point(677, 530);
+			this->yLabel->Name = L"yLabel";
+			this->yLabel->Size = System::Drawing::Size(72, 39);
+			this->yLabel->TabIndex = 8;
+			this->yLabel->Text = L"y  =";
 			// 
-			// textBox1
+			// aTextBox
 			// 
-			this->textBox1->Location = System::Drawing::Point(678, 431);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 9;
+			this->aTextBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->aTextBox->Location = System::Drawing::Point(304, 430);
+			this->aTextBox->Name = L"aTextBox";
+			this->aTextBox->Size = System::Drawing::Size(154, 45);
+			this->aTextBox->TabIndex = 9;
 			// 
-			// textBox2
+			// bTextBox
 			// 
-			this->textBox2->Location = System::Drawing::Point(696, 496);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
-			this->textBox2->TabIndex = 10;
+			this->bTextBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->bTextBox->Location = System::Drawing::Point(304, 527);
+			this->bTextBox->Name = L"bTextBox";
+			this->bTextBox->Size = System::Drawing::Size(154, 45);
+			this->bTextBox->TabIndex = 10;
 			// 
-			// textBox3
+			// xTextBox
 			// 
-			this->textBox3->Location = System::Drawing::Point(901, 412);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(100, 20);
-			this->textBox3->TabIndex = 11;
+			this->xTextBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->xTextBox->Location = System::Drawing::Point(746, 430);
+			this->xTextBox->Name = L"xTextBox";
+			this->xTextBox->Size = System::Drawing::Size(154, 45);
+			this->xTextBox->TabIndex = 11;
 			// 
-			// textBox4
+			// yTextBox
 			// 
-			this->textBox4->Location = System::Drawing::Point(916, 508);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(100, 20);
-			this->textBox4->TabIndex = 12;
+			this->yTextBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->yTextBox->Location = System::Drawing::Point(746, 527);
+			this->yTextBox->Name = L"yTextBox";
+			this->yTextBox->Size = System::Drawing::Size(154, 45);
+			this->yTextBox->TabIndex = 12;
 			// 
-			// textBox5
+			// resultTextBox
 			// 
-			this->textBox5->Location = System::Drawing::Point(727, 609);
-			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(100, 20);
-			this->textBox5->TabIndex = 13;
+			this->resultTextBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->resultTextBox->Location = System::Drawing::Point(363, 629);
+			this->resultTextBox->Name = L"resultTextBox";
+			this->resultTextBox->Size = System::Drawing::Size(905, 45);
+			this->resultTextBox->TabIndex = 13;
+			// 
+			// solveBtn
+			// 
+			this->solveBtn->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->solveBtn->Location = System::Drawing::Point(1051, 454);
+			this->solveBtn->Name = L"solveBtn";
+			this->solveBtn->Size = System::Drawing::Size(414, 105);
+			this->solveBtn->TabIndex = 14;
+			this->solveBtn->Text = L"Вычислить";
+			this->solveBtn->UseVisualStyleBackColor = true;
+			this->solveBtn->Click += gcnew System::EventHandler(this, &laba5Form::solveBtn_Click);
 			// 
 			// laba5Form
 			// 
@@ -220,20 +266,21 @@ namespace LabaForm {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ControlLightLight;
 			this->ClientSize = System::Drawing::Size(1584, 861);
-			this->Controls->Add(this->textBox5);
-			this->Controls->Add(this->textBox4);
-			this->Controls->Add(this->textBox3);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->solveBtn);
+			this->Controls->Add(this->resultTextBox);
+			this->Controls->Add(this->yTextBox);
+			this->Controls->Add(this->xTextBox);
+			this->Controls->Add(this->bTextBox);
+			this->Controls->Add(this->aTextBox);
+			this->Controls->Add(this->yLabel);
+			this->Controls->Add(this->xLabel);
+			this->Controls->Add(this->bLabel);
+			this->Controls->Add(this->aLabel);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->taskLabel);
 			this->Controls->Add(this->labaTitle);
 			this->Controls->Add(this->exitBtn);
-			this->Controls->Add(this->button1);
+			this->Controls->Add(this->backBtn);
 			this->Name = L"laba5Form";
 			this->Text = L"laba5Form";
 			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
@@ -243,7 +290,7 @@ namespace LabaForm {
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void backBtn_Click(System::Object^ sender, System::EventArgs^ e) {
 		this->Owner->Show();
 		this->Close();
 	}
@@ -251,5 +298,30 @@ namespace LabaForm {
 		this->Owner->Close();
 		this->Close();		
 	}
-	};
+
+	double laba5Func(double a, double b, double x, double y)
+	{
+		if (x < y)
+		{
+			return fmin(fmin(a - cos(x), a / (b + y)), pow(sin(x), 2));
+		}
+		else if (x < y + 5)
+		{
+			return fmax(pow(a, 3), log(pow(x, 2) + pow(y, 2)));
+		}
+		else
+		{
+			return a + pow(cos(x - y), 3);
+		}
+	}
+
+	private: System::Void solveBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+		double a, b, x, y;
+		a = Convert::ToDouble(aTextBox->Text);
+		b = Convert::ToDouble(bTextBox->Text);
+		x = Convert::ToDouble(xTextBox->Text);
+		y = Convert::ToDouble(yTextBox->Text);
+		resultTextBox->Text = Convert::ToString(laba5Func(a, b, x, y));
+	}
+};
 }
